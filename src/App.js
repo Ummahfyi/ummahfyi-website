1180c59744d5b3c44202ffc3cdf022d508d43dc9
Mona Eletr <mona.eletr13@gmail.com>
2025-06-24 23:43:42 -0400

    Update styles and add images for layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <div style={{
+      backgroundImage: "url('/realbackground.png')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      minHeight: "100vh",
+      width: "100%",
+      color: "black"
+    }}>
+    <div className="center-container">
+      <h1 className="center-heading">Simplified News For The Modern Muslim.</h1>
+    </div>
+    <div class="newsletter-box">
+      <div class="newsletter-text">Newsletter</div>
+    </div>
+    <div class="contact-box">
+      <div class="contact-text">Contact </div>
+    </div>
+    <div class="about-box">
+      <div class="about-text">About</div>
+    </div>
+    <div class="home-box">
+      <div class="home-text">Home</div>
+    </div>
+    <div class="ummahFIY-box">
+      <div class="ummahFIY-text">UmmahFIY</div>
+    </div>
+    <div class="register-box">
+      <div class="register-text">Register for our Newsletter</div>
+    </div>
+    <div class="email-box">
+      <div class="email-text">email@domain.com</div>
+    </div>
+    <div class="continue-box">
+      <div class="continue-text">Continue</div>
+    </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="/UmmahFIY.png" alt="UmmahFIY Logo" className="top-left-image" />
+    </div>
+  
+    
     </div>
   );
 }
-
 export default App;
